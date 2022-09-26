17c84abc9ae77f60133c5cad7c06ed1062e5fb0e
Fedor Sokolov <feojoker@gmail.com>
2022-09-26 20:51:14 +0400

    Add `Layout` and `Seo` components to Home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import Nav from '../components/Nav';
 import { fetchAPI } from './api/api';
-import { ApiLeftNavLeftNav, ApiRightNavRightNav, ApiGlobalGlobal } from './api/schemas';
+import Layout from "../layouts/Layout";
+import Seo from "../components/Seo";
+import { Global, Homepage, Navs } from "../types";
 
-const Home: NextPage = ({ leftNavs, rightNavs, logo }: any) => {
+type Props = {
+  homepage: Homepage,
+  rightNavs: Navs[],
+  leftNavs: Navs[],
+  logo: Global,
+}
+
+const Home = ({ leftNavs, rightNavs, logo, homepage }: Props) => {
+
   return (
-    <>
-      <Nav rightNavs={rightNavs} leftNavs={leftNavs} logo={logo} />
+    <Layout rightNavs={rightNavs} leftNavs={leftNavs} logo={logo} >
+      <Seo seo={homepage.attributes.seo} />
       <div style={{ height: '1300px' }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae felis velit. Pellentesque et sapien vel diam iaculis venenatis in eget diam. Fusce mollis erat eget dui porta porta sed et velit. Pellentesque et faucibus felis. Ut ac finibus urna. Suspendisse blandit in odio nec vestibulum. Aliquam ultricies consectetur sem eu pulvinar. Integer blandit, tortor accumsan pulvinar finibus, elit arcu imperdiet mauris, vitae volutpat arcu magna et urna. Cras a vulputate est, sit amet feugiat elit. Mauris mi ex, accumsan vitae semper non, pretium non orci. Duis aliquet, magna at congue faucibus, neque elit rhoncus dolor, non facilisis turpis enim sed ante. In nec venenatis odio. Pellentesque sagittis sed arcu ut interdum. Nulla suscipit vulputate tincidunt. Quisque non finibus sapien, ac ornare orci. Cras mollis tincidunt eros sodales pretium.
 
@@ -19,25 +28,35 @@ const Home: NextPage = ({ leftNavs, rightNavs, logo }: any) => {
 
         Suspendisse eget rutrum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer vel enim luctus, posuere odio eget, accumsan sem. Quisque id malesuada elit, gravida tristique nibh. Aliquam lectus ex, posuere vel pellentesque vitae, tincidunt sed tellus. Proin vestibulum orci in suscipit porttitor. Curabitur tellus turpis, mattis sed rhoncus quis, euismod at ipsum. Quisque id nunc nec neque semper ornare. Vivamus lacinia nisi libero, at ultricies est tempor et. Fusce eleifend ultricies ligula non ornare. Proin consectetur eu mauris sed euismod. Sed libero purus, blandit quis tincidunt vel, laoreet ac erat. Mauris sit amet sapien quis mi venenatis ultricies in bibendum erat. In ornare accumsan arcu ultricies lacinia.
       </div>
-    </>
+    </Layout>
   )
 };
 
 export async function getStaticProps() {
   // Run API calls in parallel
-  const leftNavsRes = await fetchAPI<ApiLeftNavLeftNav>("/left-navs", { populate: "*" });
-  const rightNavsRes = await fetchAPI<ApiRightNavRightNav>("/right-navs", { populate: "*" });
-  const logo = await fetchAPI<ApiGlobalGlobal>("/global", {
-    populate: {
-      favicon: "*",
-    },
-  });
+
+  const [leftNavsRes, rightNavsRes, logoRes, homepageRes] = await Promise.all([
+    fetchAPI<Navs[]>("/left-navs", { populate: "*" }),
+    fetchAPI<Navs[]>("/right-navs", { populate: "*" }),
+    fetchAPI<Global>("/global", {
+      populate: {
+        favicon: "*",
+      },
+    }),
+    fetchAPI<Homepage>("/homepage", {
+      populate: {
+        hero: "*",
+        seo: { populate: "*" },
+      },
+    }),
+  ]);
 
   return {
     props: {
       leftNavs: leftNavsRes,
       rightNavs: rightNavsRes,
-      logo: logo
+      logo: logoRes,
+      homepage: homepageRes,
     },
   };
 }
