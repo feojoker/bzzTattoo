0996dede46730136a551d6bfca8b84fd6da47070
Fedor Sokolov <feojoker@gmail.com>
2022-11-22 12:09:35 +0400

    Fix global data fetching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,12 @@ import Head from "next/head";
 import "../../styles/style.css";
 import { fetchAPI } from "./api/api";
 import { getStrapiMedia } from "./api/media";
-import { GlobalData, Lang, Navs } from "../types";
+import { CombinedGlobalData, GlobalData, Lang, Navs } from "../types";
 import { GlobalDataProvider } from "../context/GlobalDataContext";
 import { MediaQueryProvider } from "../context/MediaQueryContext";
 import { Loader } from '../components/Loader';
 
 type TProps = Pick<AppProps, "Component" | "pageProps">;
-
-type CombinedGlobalData = {
-  global: GlobalData,
-  leftNavs: Navs[],
-  rightNavs: Navs[],
-  langs: Lang[],
-}
-
 
 const MyApp = ({ Component, pageProps }: TProps) => {
 
@@ -28,8 +20,6 @@ const MyApp = ({ Component, pageProps }: TProps) => {
     rightNavs: pageProps.rightNavs,
     langs: pageProps.langs,
   }
-  console.log(globalData);
-
 
   const global: GlobalData = pageProps.global;
   const faviconHref = getStrapiMedia(global.attributes.favicon)
@@ -52,32 +42,29 @@ const MyApp = ({ Component, pageProps }: TProps) => {
     </>
   );
 };
-
 // getInitialProps disables automatic static optimization for pages that don't
 // have getStaticProps. So article, category and home pages still get SSG.
 // Hopefully we can replace this with getStaticProps once this issue is fixed:
 // https://github.com/vercel/next.js/discussions/10949
 MyApp.getInitialProps = async (context: AppContext) => {
   // Calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(context);
 
   const locale = context.router.locale;
-  const appProps = await App.getInitialProps(context);
-  // Fetch global site settings from Strapi
-  const globalRes = await fetchAPI<GlobalData>("/global-data", {
-    populate: "*"
-    // {
-    //   defaultSeo: {
-    //     populate: { shareImage: { populate: "*" } }
-    //   },
-    //   favicon: { populate: "*" },
-    //   logo: { populate: "*" },
-    // }
-  });
 
+  const globalRes = await fetchAPI<GlobalData>("/global-data", {
+    populate: {
+      defaultSeo: {
+        populate: { shareImage: { populate: "*" } }
+      },
+      favicon: { populate: "*" },
+      logo: { populate: "*" },
+    }
+  });
   const leftNavsRes = await fetchAPI<Navs[]>(`/left-navs`, { populate: "*", locale: locale });
   const rightNavsRes = await fetchAPI<Navs[]>("/right-navs", { populate: "*", locale: locale });
   const langRes = await fetchAPI<Lang[]>("/language-icons", { populate: "*" });
-  // Pass the data to our page via props
+
   return {
     ...appProps, pageProps:
     {
