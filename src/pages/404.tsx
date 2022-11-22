d75e51be9125cbe5302b190d0d0a4accc49a7804
Fedor Sokolov <feojoker@gmail.com>
2022-11-22 12:41:26 +0400

    Add data from Strapi to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,9 +13,12 @@ const Custom404Page = ({ notFound }: { notFound: NotFound }) => {
   const { global } = useContext(GlobalDataContext);
   const logoSrc = getStrapiMedia(global.attributes.logo)
 
+  const { title, description, buttonText } = notFound.attributes;
+
+
   const seo = {
-    metaTitle: notFound.attributes.title,
-    metaDescription: notFound.attributes.description,
+    metaTitle: title,
+    metaDescription: description,
   };
 
   return (
@@ -24,13 +27,11 @@ const Custom404Page = ({ notFound }: { notFound: NotFound }) => {
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
         <div className="max-w-md">
           <div className="text-5xl font-dark font-bold">404</div>
-          <p
-            className="text-2xl md:text-3xl font-light leading-normal"
-          >Sorry this page is under construction. </p>
-          <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+          <p className="text-2xl md:text-3xl font-light leading-normal">{title}</p>
+          <p className="mb-8">{description}</p>
 
           <Link href="/">
-            <a className="text-primary hover:text-white border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center font-regular">back to homepage</a>
+            <a className="text-primary hover:text-white border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center font-regular">{buttonText}</a>
           </Link>
         </div>
         <div className="max-w-[200px] mt-20 md:ml-20 md:mt-0">
