4b0dd3de7f4e3ce441ad377ec471b40f1b282151
Fedor Sokolov <feojoker@gmail.com>
2022-11-25 18:07:15 +0400

    Fix target blank issue

--- src/components/InstagramFeed.tsx
+++ src/components/InstagramFeed.tsx
@@ -9,7 +9,7 @@ function InstagramFeed({ images }: { images: any }) {
     <div className='bg-secondary'>
       <div className='grid grid-cols-4 gap-6 mx-auto py-6 max-w-6xl'>
         {filteredLastImages.map((image: any) => (
-          <a key={image.id} href={image.permalink} target='_blank'>
+          <a key={image.id} href={image.permalink} target="_blank" rel="noreferrer">
             <img className='object-cover aspect-square brightness-75 hover:filter-none transition duration-150' src={image.media_url} alt={image.caption} />
           </a>
         )
