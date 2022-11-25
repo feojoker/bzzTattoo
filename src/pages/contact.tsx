8d6e620cfde56cff32d9108d7ac5d3c788016fd1
Fedor Sokolov <feojoker@gmail.com>
2022-11-25 18:01:59 +0400

    Modify Contact page due to `ImgBanner` change

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,5 @@
 
 import React, { useContext } from 'react';
-import { GlobalData, Lang, Navs } from '../types';
 import { fetchAPI } from './api/api';
 import Layout from '../layouts/Layout';
 import FormEmail from '../components/FormEmail';
@@ -8,28 +7,25 @@ import GoogleMaps from '../components/Map';
 import ImgBanner from '../components/ImgBanner';
 import { MediaQueryContext } from '../context/MediaQueryContext';
 import { ContactPage } from '../types/pages';
+import Seo from '../components/Seo';
 
 const Contact = ({ contact }: { contact: ContactPage }) => {
   const isDesktopMedia = useContext(MediaQueryContext);
+  console.log(contact);
 
-  const formData = contact.attributes.formEmail;
+  const { formEmail, mediaBanner } = contact.attributes;
 
   return (
-    <Layout >
-      <ImgBanner src={contact.attributes.imageBanner} />
-      <div className={`bg-black h-[600px]  
-      ${isDesktopMedia
-          ? 'h-[80px]'
-          : 'h-[50px]'
-        }
-        `}></div>
+    <Layout>
+      <Seo />
+      <ImgBanner src={mediaBanner} aspectRatioH={9} aspectRatioW={21} />
       <div className="bg-secondary h-[600px]">
         <div className='flex items-center justify-between h-full'>
           <div className='relative flex flex-col items-center text-center w-[50vw] h-full' >
             <GoogleMaps />
           </div>
           <div className='w-[50vw] text-xl font-regular whitespace-pre-line p-8'>
-            <FormEmail data={formData} />
+            <FormEmail data={formEmail} />
           </div>
         </div>
       </div>
@@ -40,7 +36,10 @@ const Contact = ({ contact }: { contact: ContactPage }) => {
 export async function getStaticProps({ locale }: { locale: string }) {
 
   const contactRes = await fetchAPI<ContactPage>("/contact-page", {
-    populate: "*",
+    populate: {
+      mediaBanner: { populate: { media: "*" } },
+      formEmail: "*"
+    },
     locale: locale
   });
 
