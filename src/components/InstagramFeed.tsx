2b1498ca45ea3bd92a75e06ecfbfaed01ab80316
Fedor Sokolov <feojoker@gmail.com>
2022-11-28 13:54:04 +0400

    Replace default img to next/image with placeholder blur

--- src/components/InstagramFeed.tsx
+++ src/components/InstagramFeed.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
+import blurDataUrlShimmer from '../helpers/blurDataUrlShimmer'
 
 function InstagramFeed({ images }: { images: any }) {
 
-  const filteredLastImages = images.filter((image: any) => image.media_type !== "VIDEO").slice(0, 16)
+  const filteredLastImages = images.filter((image: any) => image.media_type !== "VIDEO").slice(0, 16);
+
+
+
 
 
   return filteredLastImages && (
@@ -10,12 +15,19 @@ function InstagramFeed({ images }: { images: any }) {
       <div className='grid grid-cols-4 gap-6 mx-auto py-6 max-w-6xl'>
         {filteredLastImages.map((image: any) => (
           <a key={image.id} href={image.permalink} target="_blank" rel="noreferrer">
-            <img
-              className='object-cover aspect-square brightness-75 hover:filter-none transition duration-150'
-              src={image.media_url}
-              alt={image.caption}
-              loading="lazy"
-            />
+            <div className='brightness-75 hover:filter-none transition duration-150'>
+              <Image
+                src={image.media_url}
+                alt={image.caption}
+                height={1}
+                width={1}
+                layout='responsive'
+                quality={100}
+                objectFit='cover'
+                placeholder="blur"
+                blurDataURL={`data:image/svg+xml;base64,${blurDataUrlShimmer('100%', '100%')}`}
+              />
+            </div>
           </a>
         )
         )}
