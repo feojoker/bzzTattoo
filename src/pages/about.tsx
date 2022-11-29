cb0bf7ab9642411811f2c4e81d623f0ab915372c
Fedor Sokolov <feojoker@gmail.com>
2022-11-29 17:56:42 +0400

    Add `scrollAnchor` to components

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,17 +3,22 @@ import React from 'react';
 import { fetchAPI } from './api/api';
 import Layout from '../layouts/Layout';
 import { AboutPage } from '../types/pages';
-import VideoBanner from '../components/VideoBanner';
+// import VideoBanner from '../components/VideoBanner';
 import Seo from '../components/Seo';
 import PhotoAndText from '../components/PhotoAndText';
+import { Loader } from '../components/Loader';
+import dynamic from 'next/dynamic';
+
+const VideoBanner = dynamic(() => import('../components/VideoBanner'), { loading: () => <Loader /> })
 
 const About = ({ about }: { about: AboutPage }) => {
   const { mediaBanner, aboutDetail, seo } = about.attributes;
+  const scrollAnchor = 'about';
   return (
     <Layout>
       <Seo seo={seo} />
-      <VideoBanner src={mediaBanner} />
-      <PhotoAndText data={aboutDetail} />
+      <VideoBanner src={mediaBanner} scrollAnchor={scrollAnchor} />
+      <PhotoAndText data={aboutDetail} scrollAnchor={scrollAnchor} />
     </Layout>
   )
 }
