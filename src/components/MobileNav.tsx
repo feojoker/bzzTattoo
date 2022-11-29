4bf1d75cafb1df1a59f64bfa93b364be4565b1a3
Fedor Sokolov <feojoker@gmail.com>
2022-11-29 17:51:58 +0400

    Clean code with svg components & some style fix

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -4,6 +4,8 @@ import { getStrapiMedia } from "../pages/api/media";
 import { Navs } from "../types";
 import { useOnScroll } from "../hooks/useOnScroll";
 import { GlobalDataContext } from "../context/GlobalDataContext";
+import Burger from "../../public/burger.svg"
+import Close from "../../public/close.svg"
 
 const MobileNav = () => {
   const { global, leftNavs, rightNavs, langs } = useContext(GlobalDataContext);
@@ -24,7 +26,7 @@ const MobileNav = () => {
 
   return (
     <>
-      <div className={`flex justify-between z-40 items-center w-full h-[50px] sticky top-0 bg-black ${scrolled ? 'bg-black' : 'bg-transparent'}`}>
+      <div className={`flex justify-between items-center w-full h-[50px] fixed top-0 bg-black z-50  ${scrolled ? 'bg-black' : 'bg-transparent'}`}>
         <Link href='/'>
           <a>
             <img className="w-[50px]" src={logoSrc} alt="logo" />
@@ -39,35 +41,10 @@ const MobileNav = () => {
             className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400"
             onClick={() => setMenuOpen(!menuOpen)}
           >
-            {menuOpen ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 text-white"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
+            {menuOpen ?
+              (<Close />) :
+              (<Burger />)
+            }
           </button>
         </div>
         <nav className="px-4">
