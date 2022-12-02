1f729348041724bf09d65353f6b535e410fbeec7
Fedor Sokolov <feojoker@gmail.com>
2022-12-02 20:12:34 +0400

    Refactor and improve `MobileNav`

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -4,15 +4,19 @@ import { getStrapiMedia } from "../pages/api/media";
 import { Navs } from "../types";
 import { useOnScroll } from "../hooks/useOnScroll";
 import { GlobalDataContext } from "../context/GlobalDataContext";
-import Burger from "../../public/burger.svg"
-import Close from "../../public/close.svg"
+import BurgerButton from "./Buttons/BurgerButton";
+import Image from "next/image";
+import MobileLanguageSwitcher from "./MobileLanguageSwitcher";
 
 const MobileNav = () => {
   const { global, leftNavs, rightNavs, langs } = useContext(GlobalDataContext);
 
   const logoSrc = getStrapiMedia(global.attributes.logo);
 
-  const scrolled = useOnScroll()
+  const scrolled = useOnScroll();
+
+  console.log(scrolled);
+
 
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -26,32 +30,37 @@ const MobileNav = () => {
 
   return (
     <>
-      <div className={`flex justify-between items-center w-full h-[50px] fixed top-0 bg-black z-50  ${scrolled ? 'bg-black' : 'bg-transparent'}`}>
-        <Link href='/'>
-          <a>
-            <img className="w-[50px]" src={logoSrc} alt="logo" />
-          </a>
-        </Link>
+      <div className={`fixed flex justify-between items-center w-full h-[80px] top-0  shadow-secondary z-50  ${scrolled && 'bg-black shadow'}`}>
+        <div className={`relative transition-all ease 
+          ${scrolled ?
+            'w-[70px] h-[70px]' :
+            'w-[100px] h-[100px] mt-[40px] ml-[20px]'
+          }`}>
+          <Link href='/'>
+            <a className="opacity-70 hover:opacity-100">
+              <Image
+                alt="logo"
+                src={logoSrc}
+                layout='fill'
+                quality={100}
+                objectFit='cover'
+              />
+            </a>
+          </Link>
+        </div>
         <div onClick={() => setMenuOpen(!menuOpen)}
           className={`fixed inset-0 z-20 bg-[rgba(255,255,255,.8)] transition-opacity ${menuOpen ? 'visible opacity-100' : 'invisible opacity-0'}`}></div>
       </div>
-      <div className={`fixed z-50 bg-black top-0 right-0 transition-all w-[calc(100%-50px)] max-w-[310px]  h-full ${menuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-        <div className="absolute right-full bg-primary p-[5px]">
-          <button
-            className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400"
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
-            {menuOpen ?
-              (<Close />) :
-              (<Burger />)
-            }
-          </button>
+      <div className={`nav-container fixed z-50 bg-black top-0 right-0 transition-all w-[calc(100%-80px)] max-w-[310px]  h-full ${menuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <div className={`absolute right-full p-[12px] ${menuOpen && 'bg-primary'}`}>
+          <BurgerButton isOpen={menuOpen} setIsOpen={setMenuOpen} />
         </div>
-        <nav className="px-4">
+        <nav className="font-modernist uppercase text-4xl px-4">
+          <MobileLanguageSwitcher langs={langs} />
           <ul>
             {leftNavs.map((category: Navs) => {
               return (
-                <li key={category.id}>
+                <li key={category.id} className="mb-2">
                   <Link href={`${category.attributes.link}`}>
                     <a className="whitespace-nowrap">{category.attributes.name}</a>
                   </Link>
@@ -62,7 +71,7 @@ const MobileNav = () => {
           <ul>
             {rightNavs.map((category: Navs) => {
               return (
-                <li key={category.id}>
+                <li key={category.id} className="mb-2">
                   <Link href={`${category.attributes.link}`}>
                     <a className="whitespace-nowrap">{category.attributes.name}</a>
                   </Link>
