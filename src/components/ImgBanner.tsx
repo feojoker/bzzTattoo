1b0305bc5fd5c815dedb6336a5d722e8a32bfdc9
Fedor Sokolov <feojoker@gmail.com>
2022-12-02 16:31:42 +0400

    Fix `ImgBanner`

--- src/components/ImgBanner.tsx
+++ src/components/ImgBanner.tsx
@@ -18,28 +18,22 @@ function ImgBanner({ src }: Props) {
   const isDesktopMedia = useContext(MediaQueryContext);
 
   return (
-
     <div className={`relative flex items-center justify-center bg-black -z-20
      ${isDesktopMedia
-        ? 'h-[70vh]'
+        ? 'h-[80vh]'
         : 'h-[40vh]'
       }`}>
       {title ? (
-        <ReactMarkdown className='absolute top-[10%] left-[10%] text-white text-5xl z-10 font-garamond uppercase'>
+        <ReactMarkdown className='absolute top-[20%] left-[10%] text-white text-5xl z-10 font-garamond uppercase'>
           {title}
         </ReactMarkdown>
       ) : null}
       {smallText ? (
-        <ReactMarkdown className='absolute bottom-[25%] right-[5%] text-white text-xl z-10 font-garamond uppercase'>
+        <ReactMarkdown className='absolute bottom-[5%] right-[10%] text-white text-xl z-10 font-garamond uppercase'>
           {smallText}
         </ReactMarkdown>
       ) : null}
-      <div className={
-        `absolute inset-x-0 bottom-0 object-cover w-full h-full -z-10
-        ${isDesktopMedia
-          ? 'top-[-80px]'
-          : 'top-[-50px]'
-        }`}>
+      <div className="absolute inset-x-0 bottom-0 object-cover w-full h-full -z-10">
         <Image
           alt="imgBanner"
           src={imgBannerSrc}
@@ -49,9 +43,6 @@ function ImgBanner({ src }: Props) {
         />
       </div>
     </div>
-
-
-
   )
 }
 
