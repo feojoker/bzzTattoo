621667fbca2c55b0638d218550515dd187d0f41c
Fedor Sokolov <feojoker@gmail.com>
2022-12-08 20:30:57 +0400

    Replace dynamic VideoBanner import with regular one

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,10 @@
 
-import React from 'react';
 import { fetchAPI } from './api/api';
 import Layout from '../layouts/Layout';
 import { AboutPage } from '../types/pages';
-// import VideoBanner from '../components/VideoBanner';
+import VideoBanner from '../components/VideoBanner';
 import Seo from '../components/Seo';
 import PhotoAndText from '../components/PhotoAndText';
-import { Loader } from '../components/Loader';
-import dynamic from 'next/dynamic';
-
-const VideoBanner = dynamic(() => import('../components/VideoBanner'), { loading: () => <Loader /> })
 
 const About = ({ about }: { about: AboutPage }) => {
   const { mediaBanner, aboutDetail, seo } = about.attributes;
