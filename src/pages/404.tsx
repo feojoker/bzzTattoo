9052f63f9474df4e75aef815c43718e873d76f96
Fedor Sokolov <feojoker@gmail.com>
2022-12-30 17:37:22 +0400

    Refactor code due to DRY principle

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,8 @@
 
-import Seo from '../components/Seo';
 import { fetchAPI } from './api/api';
+import Seo from '../components/Seo';
+import NotFoundComponent from '../components/NotFoundComponent';
 import { NotFound } from "../types/pages";
-import Link from 'next/link';
-import Image from 'next/future/image';
 
 
 const Custom404Page = ({ notFound }: { notFound: NotFound }) => {
@@ -18,26 +17,7 @@ const Custom404Page = ({ notFound }: { notFound: NotFound }) => {
   return (
     <div className="fullHeight w-screen flex items-center">
       <Seo seo={seo} />
-      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-center px-5 text-gray-700">
-        <div className="max-w-md">
-          <div className="text-5xl font-dark font-bold">404</div>
-          <p className="text-2xl md:text-3xl font-light leading-normal">{title}</p>
-          <p className="mb-8">{description}</p>
-          <Link href="/">
-            <a className="text-primary hover:text-white border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center font-regular">{buttonText}</a>
-          </Link>
-        </div>
-        <div className="md:ml-20 md:mt-0">
-          <Image
-            alt="logo404"
-            src="/logo.png"
-            height={200}
-            width={200}
-            quality={100}
-            priority
-          />
-        </div>
-      </div>
+      <NotFoundComponent data={notFound} />
     </div>
   )
 }
--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -1,14 +1,12 @@
 import { fetchAPI } from './api/api';
-import { NotFound } from "../types/pages";
 import Seo from '../components/Seo';
-import Link from 'next/link';
-import Image from 'next/future/image';
+import NotFoundComponent from '../components/NotFoundComponent';
+import { NotFound } from "../types/pages";
 
 
 const Store = ({ store }: { store: NotFound }) => {
 
-  const { title, description, buttonText } = store.attributes;
-
+  const { title, description } = store.attributes;
 
   const seo = {
     metaTitle: title,
@@ -18,26 +16,7 @@ const Store = ({ store }: { store: NotFound }) => {
   return (
     <div className="fullHeight w-screen flex items-center">
       <Seo seo={seo} />
-      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-center px-5 text-gray-700">
-        <div className="max-w-md">
-          <div className="text-5xl font-dark font-bold">404</div>
-          <p className="text-2xl md:text-3xl font-light leading-normal">{title}</p>
-          <p className="mb-8">{description}</p>
-          <Link href="/">
-            <a className="text-primary hover:text-white border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center font-regular">{buttonText}</a>
-          </Link>
-        </div>
-        <div className="max-w-[200px] md:ml-20 md:mt-0">
-          <Image
-            alt="logoStore"
-            src="/logo.png"
-            height={200}
-            width={200}
-            quality={100}
-            priority
-          />
-        </div>
-      </div>
+      <NotFoundComponent data={store} />
     </div>
   )
 }
