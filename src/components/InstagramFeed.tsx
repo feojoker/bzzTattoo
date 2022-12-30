42bc199f3a8ca91cb2ad1c747b09960d9ba68bbd
Fedor Sokolov <feojoker@gmail.com>
2022-12-30 19:57:58 +0400

    Add title and subtitle to `InstagramFeed`

--- src/components/InstagramFeed.tsx
+++ src/components/InstagramFeed.tsx
@@ -2,29 +2,43 @@ import Image from 'next/future/image';
 import { InstagramPostType } from '../types';
 import blurDataUrlShimmer from '../helpers/blurDataUrlShimmer';
 
-function InstagramFeed({ images }: { images: InstagramPostType[] }) {
+type Props = {
+  title: string,
+  subtitle: string,
+  images: InstagramPostType[]
+}
+
+function InstagramFeed({ title, subtitle, images }: Props) {
   const filteredLastImages = images.filter((image: InstagramPostType) => image.media_type !== "VIDEO").slice(0, 16);
 
   return filteredLastImages && (
-    <div className='bg-secondary'>
-      <div className='grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-6 mx-auto py-6 max-w-6xl'>
-        {filteredLastImages.map((image: InstagramPostType) => (
-          <a key={image.id} href={image.permalink} target="_blank" rel="noreferrer">
-            <div className='brightness-75 hover:filter-none transition duration-150'>
-              <Image
-                className="aspect-square object-cover"
-                src={image.media_url}
-                alt={image.caption}
-                height={400}
-                width={400}
-                quality={100}
-                placeholder="blur"
-                blurDataURL={`data:image/svg+xml;base64,${blurDataUrlShimmer(300, 300)}`}
-              />
-            </div>
-          </a>
-        )
-        )}
+    <div className='bg-secondary '>
+      <div className='container mx-auto'>
+        <div className='mx-auto pt-6 max-w-7xl lg:flex lg:items-end'>
+          <h1 className='text-7xl tracking-widest whitespace-pre-wrap'>
+            {title}
+          </h1>
+          <h2 className='text-2xl font-garamond lg:ml-8'>{subtitle}</h2>
+        </div>
+        <div className='grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-6 mx-auto py-6 max-w-6xl'>
+          {filteredLastImages.map((image: InstagramPostType) => (
+            <a key={image.id} href={image.permalink} target="_blank" rel="noreferrer">
+              <div className='brightness-75 hover:filter-none transition duration-150'>
+                <Image
+                  className="aspect-square object-cover"
+                  src={image.media_url}
+                  alt={image.caption}
+                  height={400}
+                  width={400}
+                  quality={100}
+                  placeholder="blur"
+                  blurDataURL={`data:image/svg+xml;base64,${blurDataUrlShimmer(300, 300)}`}
+                />
+              </div>
+            </a>
+          )
+          )}
+        </div>
       </div>
     </div>
   )
