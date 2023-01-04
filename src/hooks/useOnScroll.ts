7ae74f58bad5a67295526cb2da495282d79cd578
Fedor Sokolov <feojoker@gmail.com>
2023-01-04 16:34:55 +0400

    Store latest `scrollY` in localStorage

--- src/hooks/useOnScroll.ts
+++ src/hooks/useOnScroll.ts
@@ -1,32 +1,26 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
 
 export const useOnScroll = () => {
-  const [scrolled, setScrolled] = useState(false);
-
+  const [scrollPosition, setScrollPosition] = useLocalStorage('SCROLL_POSITION', 0)
 
   const onScroll: EventListener = useCallback((event: Event) => {
     const win: Window = window;
     const { scrollY } = win;
-
-    if (scrollY >= 10) {
-      setScrolled(true)
-    } else {
-      setScrolled(false)
-    }
+    setScrollPosition(scrollY)
   }, []);
 
   useEffect(() => {
     const win: Window = window;
 
-    //add eventlisteners to window
     win.addEventListener("scroll", onScroll);
     win.addEventListener("resize", onScroll);
-    // remove events on unmount to prevent a memory leak with the cleanup
+
     return () => {
       win.removeEventListener("scroll", onScroll);
       win.removeEventListener("resize", onScroll);
     }
   }, [onScroll]);
 
-  return scrolled
+  return scrollPosition >= 10
 }
