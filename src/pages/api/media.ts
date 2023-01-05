517cc8c8103dd86fa92cc86281bb4def2250d5c7
Fedor Sokolov <feojoker@gmail.com>
2023-01-05 22:37:50 +0400

    Made one more api for getting media directly from Cloudinary

--- src/pages/api/media.ts
+++ src/pages/api/media.ts
@@ -3,5 +3,13 @@ import { getStrapiURL } from "./api";
 export function getStrapiMedia(media: any) {
   const { url } = media.data.attributes;
   const imageUrl = url.startsWith("/") ? getStrapiURL(url) : url;
+
+  return imageUrl;
+}
+
+export function getCloudinaryMedia(media: any) {
+  const { url } = media.data.attributes;
+  const imageUrl = url.replace('https://res.cloudinary.com/dx2vbnmiz/image/upload', '');
+
   return imageUrl;
 }
