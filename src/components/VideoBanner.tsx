8c2b36f1bdf746ef7ce91c33089348b6a0d9f661
Fedor Sokolov <feojoker@gmail.com>
2023-01-09 16:31:01 +0400

    Add Cloudinary component for video

--- src/components/VideoBanner.tsx
+++ src/components/VideoBanner.tsx
@@ -3,6 +3,10 @@ import { MediaBanner } from "../types";
 import DoubleDown from '../../public/doubleDown.svg';
 import { Link as ScrollLink } from 'react-scroll';
 
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedVideo } from '@cloudinary/react';
+import { quality } from "@cloudinary/transformation-builder-sdk/actions/delivery"
+
 type Props = {
   src: MediaBanner,
   scrollAnchor: string,
@@ -11,9 +15,18 @@ type Props = {
 function VideoBanner({ src, scrollAnchor }: Props) {
 
   const { media, title, smallText, poster } = src;
-
-  const videoBannerSrc = getStrapiMedia(media);
   const posterSrc = getStrapiMedia(poster);
+  const videoSrc = getCloudinaryMedia(media);
+
+
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+    }
+  });
+
+  const cloudinaryVideo = cld.video(videoSrc).delivery(quality(100));
+
 
   return (
     <div className="relative font-garamond uppercase w-full mb-12 z-1 fullHeight">
@@ -38,16 +51,12 @@ function VideoBanner({ src, scrollAnchor }: Props) {
         </p>
       ) : null}
       <div className="absolute inset-0 opacity-60 bg-black w-full h-full z-10"></div>
-      <video
+      <AdvancedVideo
         className="absolute inset-0 bottom-0 object-cover w-full h-full z-5"
+        cldVid={cloudinaryVideo}
+        autoPlay muted loop playsInline
         poster={posterSrc ? posterSrc : ''}
-        loop autoPlay muted playsInline>
-        <source
-          src={videoBannerSrc}
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+      />
       <ScrollLink to={scrollAnchor} spy={true} smooth={true} offset={-100} duration={500} >
         <DoubleDown className="w-[48px] h-[48px] animate-bounce absolute text-white text-center inset-x-0 mx-auto bottom-[5%] z-20 border hover:bg-primary hover:border-primary focus:ring-2 focus:outline-none focus:ring-primary rounded-full" />
       </ScrollLink>
@@ -56,6 +65,4 @@ function VideoBanner({ src, scrollAnchor }: Props) {
 }
 
 export default VideoBanner
-
-
 
