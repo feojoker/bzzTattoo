e42c9c92df9dd449a9a7383da746ba0dd2e16884
Fedor Sokolov <feojoker@gmail.com>
2023-01-09 16:38:22 +0400

    Replace cloudName

--- src/components/VideoBanner.tsx
+++ src/components/VideoBanner.tsx
@@ -21,7 +21,7 @@ function VideoBanner({ src, scrollAnchor }: Props) {
 
   const cld = new Cloudinary({
     cloud: {
-      cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+      cloudName: "dx2vbnmiz"
     }
   });
 
