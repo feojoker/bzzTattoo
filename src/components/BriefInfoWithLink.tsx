88043650bfa6652afebb4cc2529f574cf9ba192f
Fedor Sokolov <sokolov.fedor.g@gmail.com>
2023-09-09 14:00:10 +0400

    Fix `BriefInfoWithLink` button

--- src/components/BriefInfoWithLink.tsx
+++ src/components/BriefInfoWithLink.tsx
@@ -1,4 +1,5 @@
 import { BriefInfo } from '@projectTypes/components';
+import Link from 'next/link';
 import DefaultButton from './Buttons';
 
 type Props = {
@@ -18,7 +19,11 @@ function BriefInfoWithLink({ data, scrollAnchor }: Props) {
           </h1>
           <p className='mb-4 uppercase text-3xl font-modernist'>|</p>
           <h2 className='mb-4 text-sm font-modernist uppercase tracking-wide'>{subTitle}</h2>
-          <DefaultButton tag="a" link={link} linkTitle={linkTitle} />
+          <Link href={link}>
+            <a className="text-primary hover:text-white border border-primary hover:bg-primary focus:ring-1 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center font-garamond transition duration-500 cursor-pointer uppercase">
+              {linkTitle}
+            </a>
+          </Link>
         </div>
         <div className='max-w-[600px] md:ml-24 xl:ml-0 text-xl text-center md:text-left font-garamond whitespace-pre-line'>
           {longText}
