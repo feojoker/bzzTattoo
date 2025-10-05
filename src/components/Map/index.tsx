e8c5eba9f2cd3bddaf4385ef50d784f50e81c9c5
Fedor Sokolov <fedor.sokolov@dualbootpartners.com>
2025-10-05 15:35:54 +0400

    Return Google Maps with Google Cloud Platform

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -10,24 +10,28 @@ import Image from 'next/future/image';
 type Map = google.maps.Map;
 type MapOptions = google.maps.MapOptions;
 type LatLngLiteral = google.maps.LatLngLiteral;
-
-const tattooStudioLocation = { lat: 41.701365412959866, lng: 44.794203827308415 };
-
-export default function GoogleMaps() {
-  const { locale } = useRouter();
-  const iframeLink = `https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d186.1808109245161!2d44.794253068286764!3d41.7012460780451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDQyJzA0LjgiTiA0NMKwNDcnMzkuMyJF!5e0!3m2!1sru!2sge!4v1759657555339!5m2!1s${locale}!2sge`;
-
-  return (
-    <iframe src={iframeLink} className="border-0" width="100%" height="100%" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-  );
-}
-
-
 type Props = {
   googleMapsApiKey: string;
 };
 
-function Map({ googleMapsApiKey }: Props) {
+const tattooStudioLocation = { lat: 41.701365412959866, lng: 44.794203827308415 };
+
+export default function GoogleMaps() {
+  const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+  // Fallback to iframe if Google Maps API key is not set
+  if (googleMapsApiKey === undefined) {
+    const { locale } = useRouter();
+    const iframeLink = `https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2808.7575777462635!2d44.79356862248936!3d41.70144834246813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDQyJzA0LjgiTiA0NMKwNDcnMzkuMyJF!5e0!3m2!1sru!2sge!4v1759659942705!5m2!1s${locale}!2sge`
+    return (
+      <iframe src={iframeLink} width="100%" height="100%" className="border-0" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    );
+  }
+  return <Map googleMapsApiKey={googleMapsApiKey} />;
+}
+
+function Map(props: Props) {
+  const { googleMapsApiKey } = props;
   const mapRef = useRef<Map>();
   const onMapLoad = useCallback((map: Map) => {
     mapRef.current = map;
